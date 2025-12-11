4357da5671c4d7d73763d71f88f6d4b4daff4d59
shashankkbasurbasur-source <shashankkbasurbasur@gmail.com>
2025-12-11 21:10:35 +0530

    update fix password route

--- campusguard-frontend/src/pages/Home/Labs/PasswordStrength/passwordAPI.js
+++ campusguard-frontend/src/pages/Home/Labs/PasswordStrength/passwordAPI.js
@@ -2,7 +2,7 @@ const API_BASE = import.meta.env.VITE_API_BASE || "http://127.0.0.1:5000";
 
 export const analyzePassword = async (password) => {
   try {
-    const res = await fetch(`${API_BASE}/password/analyze`, {
+    const res = await fetch("${API_BASE}/api/password/analyze", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ password }),
